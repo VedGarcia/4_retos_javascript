c8151f7641ea6bf3b7cf1bb7de9b91c6e2aefeb8
Eduardo Garcia <garciaveduar@gmail.com>
2023-06-12 14:22:38 -0400

    Se agrega css a Counter

--- counter.js
+++ counter.js
@@ -1,33 +1,48 @@
-const plus = () => {count++; createCount.innerHTML = count;};
-const minus = () => {count--; createCount.innerHTML = count;}
+const plus = () => {
+  count++;
+  createCount.innerHTML = count;
+};
+const minus = () => {
+  count--;
+  createCount.innerHTML = count;
+};
 const resetCount = () => {
-count = 0; createCount.innerHTML = count;}
-
+  count = 0;
+  createCount.innerHTML = count;
+};
 
 const divCount = document.getElementById("counter");
-
 let count = 0;
 
+const title = document.createElement('h2');
+title.textContent = 'Counter';
+title.classList.add("title");
 const createCount = document.createElement("p");
+createCount.classList.add('counter');
 createCount.innerHTML = count;
 
+const buttonContain = document.createElement('div');
+buttonContain.classList.add('buttonContain');
+
 const buttonPlus = document.createElement("button");
-buttonPlus.textContent = "Agregar";
+buttonPlus.classList.add("button");
+buttonPlus.textContent = "+";
 buttonPlus.addEventListener("click", () => {
   plus();
 });
 
-
 const buttonReset = document.createElement("button");
-buttonReset.textContent = "Resetear";
-buttonReset.addEventListener("click",() => {
-resetCount()
-})
-
-const buttonMinus = document.createElement("button");
-buttonMinus.textContent = "Disminuir";
-buttonMinus.addEventListener("click", ()=>{
-    minus();
+buttonReset.classList.add("button");
+buttonReset.textContent = "Reset";
+buttonReset.addEventListener("click", () => {
+  resetCount();
 });
 
-divCount.append(createCount, buttonPlus, buttonReset, buttonMinus);
+const buttonMinus = document.createElement("button");
+buttonMinus.classList.add("button");
+buttonMinus.textContent = "-";
+buttonMinus.addEventListener("click", () => {
+  minus();
+});
+buttonContain.append(buttonMinus, buttonReset, buttonPlus);
+divCount.append(title, createCount, buttonContain);
